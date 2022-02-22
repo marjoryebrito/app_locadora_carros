251a449a7e80d174f7ed41dab0e8505f7be6e16a
marjoryebrito <marjoryebrito@protonmail.com>
2022-02-22 08:28:39 -0300

    Rotas e a diferença entre Route::resource e Route::apiResource

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -19,16 +19,6 @@ class ClienteController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \App\Http\Requests\StoreClienteRequest  $request
@@ -50,16 +40,7 @@ class ClienteController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Cliente  $cliente
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Cliente $cliente)
-    {
-        //
-    }
+ 
 
     /**
      * Update the specified resource in storage.
--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('/', function () {
     return ['Chagemos aqui' => 'SIM'];
 });
+
+
+Route::apiResource('carro', 'App\Http\Controllers\CarroController');
+Route::apiResource('cliente', 'App\Http\Controllers\ClienteController');
+Route::apiResource('locacao', 'App\Http\Controllers\LocacaoController');
+Route::apiResource('marca', 'App\Http\Controllers\MarcaController');
+Route::apiResource('modelo', 'App\Http\Controllers\ModeloController');
