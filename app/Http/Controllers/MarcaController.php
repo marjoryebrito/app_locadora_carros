fb2542421106720ddb3617d2d8e8281ae1a6f73c
marjoryebrito <marjoryebrito@protonmail.com>
2022-02-22 10:22:38 -0300

    Atualizando registros via PUT e PATCH

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -72,7 +72,9 @@ class MarcaController extends Controller
      */
     public function update(Request $request, Marca $marca)
     {
-        //
+        $marca->update($request->all());
+        
+        return $marca;
     }
 
     /**
