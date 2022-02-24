6438d2d83a8d1ebffc626cd4ae64d1db2e493be3
marjoryebrito <marjoryebrito@protonmail.com>
2022-02-23 21:27:16 -0300

    Filtros - Aplicando os filtros ao resource Marca.

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -18,9 +18,45 @@ class MarcaController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $marcas = $this->marca->with('modelos')->get();
+
+
+        $marcas = array();
+
+        if($request->has('atributos_modelo')){
+            $atributos_modelo = $request->atributos_modelo;
+
+            $marcas = $this->marca->with('modelos:id,'.$atributos_modelo);
+        }else{
+            $marcas = $this->marca->with('modelos');
+        }
+
+
+        if($request->has('filtro')){
+          
+
+            $filtros = explode(';', $request->filtro);
+
+            foreach($filtros as $key =>$condicao){
+
+                $c = explode(':', $request->filtro);
+                $marcas = $marcas->where($c[0], $c[1], $c[2]);
+
+            }
+
+        }
+
+
+        if($request->has('atributos')){
+            $atributos = $request->atributos;
+
+            $marcas = $marcas->selectRaw($atributos)->get();
+        }else{
+            $marcas = $marcas->get();
+        }
+
+       // $marcas = $this->marca->with('modelos')->get();
 
         return response()->json($marcas, 200);
     }
