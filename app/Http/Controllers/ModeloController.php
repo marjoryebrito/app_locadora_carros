1c809fd9ddbc3fce1c06f64e532e02e5d6ee9d9f
marjoryebrito <marjoryebrito@protonmail.com>
2022-02-24 19:06:15 -0300

    Repository Design Pattern - Implementando um Abstract Repository.

--- app/Http/Controllers/ModeloController.php
+++ app/Http/Controllers/ModeloController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Modelo;
 use Illuminate\Support\Facades\Storage;
+use App\Repositories\ModeloRepository;
 
 use Illuminate\Http\Request;
 
@@ -21,42 +22,39 @@ class ModeloController extends Controller
      */
     public function index(Request $request)
     {
+        $modeloRepository = new ModeloRepository($this->modelo);
+
+
+
         $modelos = array();
 
         if($request->has('atributos_marca')){
-            $atributos_marca = $request->atributos_marca;
 
-            $modelos = $this->modelo->with('marca:id,'.$atributos_marca);
+            $atributos_marca = 'marca:id,'.$request->atributos_marca;
+
+            $modeloRepository->selectAtributosRegistrosRelacionados($atributos_marca);
         }else{
-            $modelos = $this->modelo->with('marca');
+           
+            $modeloRepository->selectAtributosRegistrosRelacionados('marcas');
         }
+
 
 
         if($request->has('filtro')){
           
 
-            $filtros = explode(';', $request->filtro);
-
-            foreach($filtros as $key =>$condicao){
-
-                $c = explode(':', $request->filtro);
-                $modelos = $modelos->where($c[0], $c[1], $c[2]);
-
-            }
+            $modeloRepository->filtro($request->filtro);
 
         }
 
         if($request->has('atributos')){
-            $atributos = $request->atributos;
+           
+            $modeloRepository->selectAtributos($request->atributos);    
 
-            $modelos = $modelos->selectRaw($atributos)->get();
-        }else{
-            $modelos = $modelos->get();
         }
 
-       
 
-        return response()->json($modelos, 200);
+        return response()->json($modeloRepository->getResultado(), 200);
     }
 
     /**
--- app/Repositories/ModeloRepository.php
+++ app/Repositories/ModeloRepository.php
@@ -1,41 +1,11 @@
 <?php
 
 namespace App\Repositories;
-use Illuminate\Database\Eloquent\Model;
-use App\Repositories\ModeloRepository;
 
 
-class ModeloRepository{
 
-    public function __construct(Model $model){
-        $this->model = $model;
-    }
+class ModeloRepository extends AbstractRepository{
 
-
-    public function selectAtributosRegistrosRelacionados($atributos){
-
-       $this->model =  $this->model->with($atributos);
-    }
-
-    public function filtro($filtros){
-        $filtros = explode(';', $filtros);
-
-        foreach($filtros as $key =>$condicao){
-
-            $c = explode(':', $condicao);
-            $this->model = $this->model->where($c[0], $c[1], $c[2]);
-
-        }
-    }
-
-    public function selectAtributos($atributos){
-        $this->model = $this->model->selectRaw($atributos);
-    }
-
-    public function getResultado(){
-        return $this->model->get();
-    }
-    
 
 }
 
